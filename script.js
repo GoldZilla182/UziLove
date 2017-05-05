68fc402d4b57dd95365ee09de332b5d73e818e01
Alessandro Baccin <Alessandro Baccin>
2017-05-05 21:54:47 +0100

    final website

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 $('.carousel').slick();
 
-var num = 98; //number of pixels before modifying styles
+var num = 768.59; //number of pixels before modifying styles
 var scroll = false;
 document.onscroll = function() {
     if( $(window).scrollTop() > num ) {
-        $('.fixed-nav').addClass('fixed-div');
+        $('.fixed-nav').addClass('scrolling');
     }
     else {
-        $('.fixed-nav').removeClass('fixed-div');
+        $('.fixed-nav').removeClass('scrolling');
     }
 };
 
-$('.accordion li').on({
-    mouseenter: function () {
-        $(this).children('.accordion-item-div').addClass('open');
-        $('#whiteHeader').addClass('wOpen');
-        $('#logo').addClass('logopen');
-        $('.fixed-nav').addClass('fxd-nav-open');
-    },
-    mouseleave: function () {
-        $(this).children('.accordion-item-div').removeClass('open');
-        $('#whiteHeader').removeClass('wOpen');
-        $('#logo').removeClass('logopen');
-        $('.fixed-nav').removeClass('fxd-nav-open');
-    }
-});
